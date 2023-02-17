eb39fa31b14613c1a71bb1c4305423db33a2f62d
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-17 15:43:33 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    char str[] = "\"Programming is like building a multilingual puzzle";
-    puts(str);
-    return 0;
+/**
+ * This is the puts exercise
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
 }
