c8c99ea1a8a6c896dfc5b7e74b404e8d40dd69c9
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-17 17:35:41 -0800

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints to standard error
+ *
+ * Return: 1 if successful
+ */
+int main (void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
