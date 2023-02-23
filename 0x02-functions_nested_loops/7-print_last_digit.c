ba4e3eebe3a0c1cee17a97c14719bab19507bb9b
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 20:14:26 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,9 +14,9 @@ int print_last_digit(int n)
 	
 	if (last_digit < 0)
 		{
-		last_digit = -last_digit;
+		last_digit = last_digit * -1;
 		}
 		
-	putchar(last_digit + '0');
+	_putchar(last_digit + '0');
 	return (last_digit);
 	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
+#endif
