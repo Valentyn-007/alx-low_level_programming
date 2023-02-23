808de141e4d17aced7ed30f7912c797aa926ac6f
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 20:04:53 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *
+ * @n: param  for fn
+ *
+ * Return: 0 if successful
+ */
+int print_last_digit(int n)
+	{
+	int last_digit = n % 10;
+	
+	if (last_digit < 0)
+		{
+		last_digit = -last_digit;
+		}
+		
+	putchar(last_digit + '0');
+	return (last_digit);
+	}
