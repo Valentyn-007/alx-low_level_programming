48f54fc791e33faa96fab880a3344ded26fcbbfb
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 19:22:28 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - checks for alphabets
+ *
+ * @n - parameter of the _abs function
+ *
+ * Return: 0 if successful
+ */
+int _abs(int n)
+	{
+	if (n < 0)
+		{
+		return -n;
+		}
+	else
+		{
+		return n;
+		}
+	}
