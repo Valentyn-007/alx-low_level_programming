a73d3c49884ff549b2560110b8f87a2588768d61
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 17:40:54 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,22 +3,20 @@
 
 /**
  * main - main entry point of program
- * print_alphabet - prints the letters of the alphabet in lowercase
+ * 
  *
  * Return: 0 if successful
  */
 
 int main(void)
 	{
-	void print_alphabet(void)
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
 		{
-		char c;
-		for (c = 'a'; c <= 'z'; c++)
-			{
-			_putchar(c);
-			}
-		_putchar('\n');
+		_putchar(c);
 		}
+	_putchar('\n');
+	}
 
 	return (0);
 	}
