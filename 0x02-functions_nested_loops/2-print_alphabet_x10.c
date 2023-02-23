33a2e4cd0d0548aedd7767f24a8c437f68b21988
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 18:33:13 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,14 +12,14 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char c;
 
-	while(i<11)
+	while (i < 11)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 
 		{
 		_putchar(c);
-		_putchar('\n');
 		}
+		_putchar('\n');
 	i++;
 	}
 	}
