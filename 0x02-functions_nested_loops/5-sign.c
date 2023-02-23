eccb981f2c3980a20a95c697a1fe6219a0a7d09d
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 19:11:20 -0800

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - checks for alphabets
+ *
+ * @
+ *
+ * Return: 0 if successful
+ */
+int print_sign(int n)
+	{
+	if (n > 0)
+		{
+		putchar('+');
+		return 1;
+		}
+	else if (n == 0)
+		{
+		putchar('0');
+		return 0;
+		}
+	else
+		{
+		putchar('-');
+		return -1;
+		}
+	}
