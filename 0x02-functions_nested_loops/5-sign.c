6b6386f1ee99c546509dd77b02141f3a0a79b648
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 19:37:23 -0800

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 		{
 		putchar('+');
-		return (1);
+		putchar('1');
 		}
 	else if (n == 0)
 		{
 		putchar('0');
-		return (0);
+		putchar('0');
 		}
 	else
 		{
 		putchar('-');
-		return (-1);
+		putchar('1');
 		}
 	return (0);
 	}
