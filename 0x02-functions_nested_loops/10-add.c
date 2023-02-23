3c4b299e28055fea9c6e5ac34c66e800e1f7e8bf
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 20:58:46 -0800

    9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ *
+ * @int : param for add fn
+ *
+ * Return: 0 if successful
+ */
+ int add(int a, int b)
+ 	{
+	int result = a + b;
+
+	return (result);
+	}
