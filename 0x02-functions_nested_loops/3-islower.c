1ed6218649bd312a87875c1d455f0e856f7dd269
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 18:45:35 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - main access point into program
+ *
+ * Return: 0 if successful
+ */
+ int _islower(int c)
+ 	{
+	if (c >= 'a' && c <= 'z')
+	{
+	return 1;
+	}
+	else
+	{
+	return 0;
+	}
+	}
