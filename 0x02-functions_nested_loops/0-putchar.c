4a30c2a24096ac6254219df8b5699020df6fda47
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-22 17:16:35 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints "_putchar without using printf"
+ *
+ * Return: 0 if successful
+ */
+int main(void) 
+	{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+	}
