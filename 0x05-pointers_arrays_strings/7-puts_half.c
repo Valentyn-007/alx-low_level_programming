72e4479c0fe10469e424078a850c7ea5767626bf
Valentyn-007 <iam.ugovalentyn@gmail.com>
2023-02-27 21:12:34 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* puts_half - prints half a string
+* @str: our input
+*/
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0)
+	{
+		len++;
+		str++;
+	}
+
+	str -- (len / 2);
+	while
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+
+}
